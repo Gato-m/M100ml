4546560bf1e49212818862cd0684ccfe87962872
Maris-Pidiks <maris@divikiwi.lv>
2026-05-20 14:11:16 +0300

    Clean template + add 3-tab router structure

---
+++ src/app/(tabs)/info.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function InfoScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Info</Text>
+    </View>
+  );
+}
